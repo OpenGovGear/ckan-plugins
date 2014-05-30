1222e375a9ccf3af456a76a1e5d5f5bdf9fcefec
jared-brown <jaredbrown79@gmail.com>
2014-05-29 23:31:33 -0700

    Just testing out this old example theme

--- ckanext-example_theme/setup.py
+++ ckanext-example_theme/setup.py
@@ -6,15 +6,15 @@ version = '0.0'
 setup(
     name='ckanext-example_theme',
     version=version,
-    description="The example theme extension from CKAN theming tutorial",
+    description="",
     long_description='''
     ''',
     classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords='',
-    author='Jared Brown',
-    author_email='jaredbrown79@gmail.com',
-    url='www.opengovgear.com',
-    license='GNU Affero GPL',
+    author='',
+    author_email='',
+    url='',
+    license='',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['ckanext', 'ckanext.example_theme'],
     include_package_data=True,
@@ -25,6 +25,6 @@ setup(
     entry_points='''
         [ckan.plugins]
         # Add plugins here, e.g.
-        # myplugin=ckanext.example_theme.plugin:PluginClass
+        example_theme=ckanext.example_theme.plugin:ExampleThemePlugin
     ''',
 )
